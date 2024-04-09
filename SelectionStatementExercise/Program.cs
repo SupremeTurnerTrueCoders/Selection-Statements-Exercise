d3fb3f60e211fde23e32630a9def6e752e7c9adc
SupremeTurnerTrueCoders <email@example.com>
2024-04-09 01:22:25 -0400

    Selection statements exercise complete

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,79 @@
-﻿namespace SelectionStatementExercise
+﻿using System.Reflection.Metadata;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Pick a number from 1-1000");
+
+            var r = new Random();
+            var favNumber = r.Next(1, 10);
+
+
+
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine($"{userInput} is to low");
+
+
+            }
+        }
+    }
+              else if (userInput >favNumber)
+              { 
+}
+                Console.WriteLine($"{userInput} is to High");
+
+              }
+                else 
+                { 
+
+                   Console.WriteLine($"You guessed it!");
+ 
+                }
+
+
+namespace SelectionStatementExercise part 2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what is your favorite school subject?");
+
+            var subject = Console.ReadLine();
+
+            switch (subject) 
+            {
+            case "Math":
+                Console.WriteLine("Math is a tough subject!");
+                break;
+
+            case "Science":
+                Console.WriteLine("Science is a tough subject!");
+                break;
+
+            case "English":
+                Console.WriteLine("English is a tough subject!");
+                break;
+
+            case "Social Studies":
+                Console.WriteLine("History is a tough subject!");
+                break;
+
+            case "Gym":
+                Console.WriteLine("Gym is a tough subject!");
+                break;
+
+                default:
+                Console.WriteLine("Math is a tough subject!");
+
         }
     }
 }
+            }
+
