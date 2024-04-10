4cedeb9fa53d92668ba3538cb3e40d83f488b5c5
SupremeTurnerTrueCoders <email@example.com>
2024-04-10 17:15:50 -0400

    Finished Selection statement exercise

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -6,74 +6,58 @@ namespace SelectionStatementExercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Pick a number from 1-1000");
+            Console.WriteLine("Pick a number from 1-10");
 
             var r = new Random();
             var favNumber = r.Next(1, 10);
-
-
 
             var userInput = int.Parse(Console.ReadLine());
 
             if (userInput < favNumber)
             {
                 Console.WriteLine($"{userInput} is to low");
-
-
             }
-        }
-    }
-              else if (userInput >favNumber)
-              { 
-}
+            else if (userInput > favNumber)
+            {
                 Console.WriteLine($"{userInput} is to High");
-
-              }
-                else 
-                { 
-
-                   Console.WriteLine($"You guessed it!");
- 
-                }
-
-
-namespace SelectionStatementExercise part 2
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
+            }
+            else
+            {
+                Console.WriteLine($"You guessed it!");
+            }
             Console.WriteLine("what is your favorite school subject?");
 
             var subject = Console.ReadLine();
 
-            switch (subject) 
+            switch (subject)
             {
-            case "Math":
-                Console.WriteLine("Math is a tough subject!");
-                break;
+                case "Math":
+                    Console.WriteLine("Math is a tough subject!");
+                    break;
 
-            case "Science":
-                Console.WriteLine("Science is a tough subject!");
-                break;
+                case "Science":
+                    Console.WriteLine("Science is a tough subject!");
+                    break;
 
-            case "English":
-                Console.WriteLine("English is a tough subject!");
-                break;
+                case "English":
+                    Console.WriteLine("English is a tough subject!");
+                    break;
 
-            case "Social Studies":
-                Console.WriteLine("History is a tough subject!");
-                break;
+                case "Social Studies":
+                    Console.WriteLine("History is a tough subject!");
+                    break;
 
-            case "Gym":
-                Console.WriteLine("Gym is a tough subject!");
-                break;
+                case "Gym":
+                    Console.WriteLine("Gym is a tough subject!");
+                    break;
 
                 default:
-                Console.WriteLine("Math is a tough subject!");
+                    Console.WriteLine("Math is a tough subject!");
+                    break;
 
+            }
         }
     }
 }
-            }
+
 
